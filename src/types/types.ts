b90bcd398aed28a2e40b174528db2a98f76e6359
Oscar Carrasco <anubis.carrasco03@gmail.com>
2026-02-05 20:29:59 -0600

    refactor(types): normalize AuthUser model to new shape

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,19 +16,33 @@ export interface LoginResponse {
 
 export interface AuthUser {
   message: string;
-  data: {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    email_verified_at: string | null;
-    username: string;
-    active: boolean;
-    last_login_at: string | null;
-    created_by: string | null;
-    created_at: string;
-    updated_by: string | null;
-    updated_at: string;
-    role: string;
-  };
+  id: number;
+  fullName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  active: boolean;
+  lastLoginAt: string | null;
+  updatedBy: string | null;
+  updatedAt: string;
+  company: UserCompany | null;
+  roles: Role[];
+  permissions: Permission[] | null;
+}
+
+export interface Role {
+  id: string;
+  name: string;
+}
+
+export interface Permission {
+  id: string;
+  name: string;
+}
+
+export interface UserCompany {
+  id: string;
+  name: string;
+  slug: string;
 }
